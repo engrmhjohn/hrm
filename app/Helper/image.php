e9b82a85b69fa5184ba4c893159072ef8bed04c2
Mehedi Hasan John <111722147+engrmhjohn@users.noreply.github.com>
2023-06-04 17:09:42 +0600

    make imageupload function as nullable

--- app/Helper/image.php
+++ app/Helper/image.php
@@ -1,19 +1,31 @@
 <?php
 
+// function image_upload($image, $title = '')
+// {
+//     if ($image->extension() == 'pdf') {
+//         $imageName = 'hrm' . '-' . str_replace(' ', '-', $title) .'-'. rand(1, 999) . '.' . $image->extension();
+//         // Public Folder
+//         $image->move(public_path('storage/uploads'), $imageName);
+//         return 'storage/uploads/' . $imageName;
+//     } else {
+//         $imageName = time() . rand(1, 999) . '.' . $image->extension();
+//         // Public Folder
+//         $image->move(public_path('storage/uploads'), $imageName);
+//         return 'storage/uploads/' . $imageName;
+//     }
+// }
+
 function image_upload($image, $title = '')
 {
-    if ($image->extension() == 'pdf') {
-        $imageName = 'hrm' . '-' . str_replace(' ', '-', $title) .'-'. rand(1, 999) . '.' . $image->extension();
-        // Public Folder
-        $image->move(public_path('storage/uploads'), $imageName);
-        return 'storage/uploads/' . $imageName;
-    } else {
-        $imageName = time() . rand(1, 999) . '.' . $image->extension();
-        // Public Folder
-        $image->move(public_path('storage/uploads'), $imageName);
-        return 'storage/uploads/' . $imageName;
-    }
+    if ($image) {
+            $imageName = time() . rand(1, 999) . '.' . $image->extension();
+            // Public Folder
+            $image->move(public_path('storage/uploads'), $imageName);
+            return 'storage/uploads/' . $imageName;
+        }
+    return null;
 }
+
 
 function delete_image($image_name, $path = '')
 {
